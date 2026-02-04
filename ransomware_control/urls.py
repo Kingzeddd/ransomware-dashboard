a261c38a0e27c11d59e37d4b861df8f5cb8f4bc0
Kingzeddd <dzanwa2003@gmail.com>
2026-02-04 20:39:18 +0000

    Fix: Correct dashboard URLs paths

--- ransomware_control/urls.py
+++ ransomware_control/urls.py
@@ -1,12 +1,8 @@
-"""
-URL configuration for ransomware_control project.
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
-    path('api/', include('dashboard.api_urls')),
+    path('', include('ransomware_control.dashboard.urls')),
+    path('api/', include('ransomware_control.dashboard.api_urls')),
 ]
